816907f0f3df85c6decf240dac7258b89348a8e8
BoonDoPark <pbd0416@gmail.com>
2023-09-16 14:46:55 +0900

    config 파일 숨김

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/TypeOrmConfig';
+import { typeOrmConfig } from '../config/TypeOrmConfig';
 import { SpaceModule } from './space/space.module';
 import { AuthModule } from './auth/auth.module';
 
